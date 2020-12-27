51de1aa64e17de17ec5622b6af2840a83816412b
Austin Conlon <austinconlon@icloud.com>
2020-12-27 00:19:05 -0800

    Initial Commit

---
+++ Harmon/HarmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonApp.swift
+//  Harmon
+//
+//  Created by Austin Conlon on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct HarmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
